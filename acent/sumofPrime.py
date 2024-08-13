1354b0d9ff5e0ff88192ad45c46df18653ec792e
Prince mishra <113469532+Aj-ninja007@users.noreply.github.com>
2024-08-14 01:21:21 +0530

    Update sumofPrime.py

--- acent/sumofPrime.py
+++ acent/sumofPrime.py
@@ -15,3 +15,36 @@ def sumofPrime(n):
 n=10
 print(sumofPrime(n))
 
+
+...............................................or.................................................................................................................................
+
+
+
+def sumofPrime(n):
+    sum=0
+    for i in range(2,n):
+        a=True
+        for j in range(2,int(i ** 0.5) + 1):#square root using
+           
+            if i%j==0:
+                a=False
+                break
+        if a:
+            sum+=i
+            print(i)
+        
+    return sum
+n=10
+print(sumofPrime(n))
+
+
+
+
+
+
+
+
+
+
+
+
