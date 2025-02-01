ff5003ca2e12d92aa2ac4ac723d2740fde979117
Prince mishra <113469532+Aj-ninja007@users.noreply.github.com>
2025-02-02 00:19:49 +0530

    Update primePatter.py

--- patter/primePatter.py
+++ patter/primePatter.py
@@ -30,3 +30,153 @@ def print_prime_pattern(n):
 
 # Print the pattern with the first 5 numbers including 1
 print_prime_pattern(5)
+
+
+
+
+
+
+
+
+
+def pattern():
+    n=5
+    for i in range(n-1):
+        for j in range(i+1):
+            print("*",end=" ")
+        print(" ")
+pattern()
+def pattern():
+    n=5
+    for i in range(n,0,-1):
+                                        
+        for k in range(i-1):
+            print("*",end=" ")
+        print(" ")
+pattern()
+* * * *  
+* * *  
+* *  
+*  
+            
+def pattern():
+    n=5
+    for i in range(1,n+1):
+        for k in range(n-i):
+            print(" ",end=" ")
+        for j in range(1,2*i):
+            print("*",end=" ")
+        print(" ")
+    for i in range(n-1,0,-1):
+        for k in range(n-i):
+            print(" ",end=" ")
+        for j in range(1,2*i):
+            print("*",end=" ")
+        print(" ")
+            
+pattern()
+
+
+        *  
+      * * *  
+    * * * * *  
+  * * * * * * *  
+* * * * * * * * *  
+* * * * * * * * *  
+  * * * * * * *                  
+    * * * * *  
+      * * *  
+        *  
+
+
+
+
+
+        *  
+      * * *  
+    * * * * *  
+  * * * * * * *  
+* * * * * * * * *  
+  * * * * * * *  
+    * * * * *  
+      * * *  
+        *  
+            
+            
+# def pater():
+#     n=5
+#     for i in range(1,n+1):
+#         for j in range(n-i):
+#             print(" ",end=" ")
+#         for k in range(1,2*i):
+#             print("*",end=" ")
+#         print( )
+#     for i in range(n-1,0,-1):
+#         for j in range(n-i):
+#             print(" ",end=" ")
+#         for k in range(1,2*i):
+#             print("*",end=" ")
+#         print( )
+        
+        
+# pater()
+
+# def second(l):
+#     lar=0
+#     s=0
+#     lar=l[0]
+#     for i in range(1,len(l)):
+#         if l[i]>lar:
+#             lar=l[i]
+#     for i in range(len(l)):
+#         if l[i]>s and l[i]!=lar:
+#             s=l[i]
+#     return s
+# l=[4,3,6,2,8,9]
+# print(second(l))
+
+def second(l):
+    lar=0
+    s=0
+    lar=l[0]
+    for i in range(1,len(l)):           #optmised approach
+        if l[i]>lar:
+            s=lar
+            lar=l[i]
+        elif lar>l[i] and l[i]>s:
+            s=l[i]
+    return s
+l=[4,3,6,2,8,9]
+print(second(l))
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
